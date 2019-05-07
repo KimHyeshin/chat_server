1359b3c0c51301d9f949e7df9856113e604ab4df
김혜신 <hyeshin2530@gmail.com>
2019-05-07 14:44:29 +0900

    server : all chat socket 연동 코드 작성

--- sockets/index.js
+++ sockets/index.js
@@ -1,9 +1,37 @@
+var mapNameId = {}
+var mapIdName = {}
 
 module.exports = function(io){
   io.on('connection', function(socket){
     socket.on('sendMsg', (msg) => {
       const ret = `[${new Date().format('HH:mm:ss')}] : ${msg}`
       socket.emit('sendMsg', ret)
+    })
+    socket.on('enter', (name) => {
+      const id = socket.id
+      if (mapIdName.hasOwnProperty(name)) {
+        const ret = `already user_name : ${name}`
+        socket.emit('enter', ret)
+        console.log(ret)
+      } else if (mapNameId.hasOwnProperty(id)) {
+        const ret = `already user_id : ${id}`
+        socket.emit('enter', ret)
+        console.log(ret)
+      } else {
+        mapNameId[name] = id
+        mapIdName[id] = name
+        socket.emit('enter', false)
+        console.log(`new user(${name}) : ${id}`)
+        const ret = `[${new Date().format('HH:mm:ss')}] '${name}' 님이 입장하였습니다.`
+        socket.emit('sendAll', ret)
+        socket.broadcast.emit('sendAll', ret)
+      }
+    })
+    socket.on('sendAll', (msg) => {
+      const id = socket.id
+      const ret = `[${new Date().format('HH:mm:ss')}] ${mapIdName[id]} : ${msg}`
+      socket.emit('sendAll', ret)
+      socket.broadcast.emit('sendAll', ret)
     })
     socket.on('disconnect', (s) => {
       console.log(`disconnected`)
